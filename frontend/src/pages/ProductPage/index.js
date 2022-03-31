53b857ae19d1056b8afc0cfa1c4e0d33644a40a1
VictoriaBuhai <tori001.b@gmail,com>
2022-03-31 04:31:06 +0300

    add button

--- frontend/src/pages/ProductPage/index.js
+++ frontend/src/pages/ProductPage/index.js
@@ -1,10 +1,14 @@
-import { useEffect, useState } from "react";
+import { Button } from "@mui/material";
+import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getProductById } from "../../api/api";
+import { OrderFunction } from "../../App";
 import "./styles.css";
 
 export const ProductPage = () => {
   const params = useParams();
+
+  const { addButton } = useContext(OrderFunction);
 
   const [product, setProduct] = useState({});
 
@@ -21,10 +25,11 @@ export const ProductPage = () => {
       <div className="info">
         <img src={product.imagePath} alt="img" />
         <div className="text">
-          <h1>
-            {product.name} {product.price}$
-          </h1>
+          <h1>{product.name}</h1>
+          <h2>Price: {product.price}$</h2>
+          <h3>Information</h3>
           <p>{product.information}</p>
+          <Button onClick={() => addButton(product)}>Add to order</Button>
         </div>
       </div>
     </>
